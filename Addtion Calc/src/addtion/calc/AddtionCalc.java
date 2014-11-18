b7cfc0bd7f2c82a78db318a5d357587acfac8a43
DanDaniel0 <Daniel@DanielAlhadeff.com>
2014-11-18 16:46:07 -0500

    I finished the test calc

--- Addtion Calc/src/addtion/calc/AddtionCalc.java
+++ Addtion Calc/src/addtion/calc/AddtionCalc.java
@@ -1,18 +1,20 @@
 
 package addtion.calc;
-
+import java.util.Scanner;
 public class AddtionCalc {
 
-    Scanner new Scanner Scanner
+    public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
       
-        
-        
-       
+        System.out.println("First num to add");
+        double val1 = scan.nextDouble();
+        System.out.println("Second num to add");
+        double val2 = scan.nextDouble();
+        System.out.println("="+Add(val1,val2));
     }
-    public static double Subtract(double num1, double num2)
+    public static double Add(double num1, double num2)
     {
-        return(num1-num2);
+        return(num1+num2);
     }
     
 }
