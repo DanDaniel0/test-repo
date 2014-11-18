c1827a4075eafcc13f0fde9c5265b84721b25aaf
DanDaniel0 <Daniel@DanielAlhadeff.com>
2014-11-18 16:34:38 -0500

    stuff is a test

---
+++ Addtion Calc/src/addtion/calc/AddtionCalc.java
@@ -0,0 +1,18 @@
+
+package addtion.calc;
+
+public class AddtionCalc {
+
+    Scanner new Scanner Scanner
+    public static void main(String[] args) {
+      
+        
+        
+       
+    }
+    public static double Subtract(double num1, double num2)
+    {
+        return(num1-num2);
+    }
+    
+}
